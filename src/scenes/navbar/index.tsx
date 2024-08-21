b2e9e196bfc08c4e76d97a83fbbd3a8eb6e237f9
Richard <richardvu93@gmail.com>
2024-08-21 12:01:32 -0700

    pagelayout

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,26 @@
+import Grandis from "../../assets/grandis.png";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div>
-      <div className="mx-auto flex flex-col p-0">hello</div>
-      <div className="m-0 flex p-1 underline">hello</div>
-      <div className="m-0 flex p-1 underline">hello</div>
-      <div>hello</div>
+      {/* NAVBAR */}
+      <div className="flex items-center justify-between px-10 py-5">
+        {/* LEFT SIDE  */}
+        <div>
+          <a href="/" className="">
+            <img
+              className="h-[80px] w-[250px]"
+              src={Grandis}
+              alt="grandis-logo"
+            />
+          </a>
+        </div>
+        {/* RIGHT SIDE  */}
+        <div>
+          <Link to={"food"}>Food</Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/scenes/shared/pageLayout.tsx
+++ src/scenes/shared/pageLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../navbar/index";
+import Footer from "../footer";
 
 const PageLayout = () => {
   return (
     <div>
-      PageLayout
-      <div>
-        <Outlet />
-      </div>
+      <Navbar />
+      <Outlet />
+      {/* <Footer /> */}
     </div>
   );
 };
